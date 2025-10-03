a4dcaa9c855c75d8f122123a8c0325082a0f217a
Ruslan Idrisov <r.idrisov@alphaone.ru>
2025-10-03 04:00:59 +0300

    add watcher tests

--- packages/watcher/__tests__/index.ts
+++ packages/watcher/__tests__/index.ts
@@ -1,4 +1,26 @@
-describe('createProvider', () => {
-  test('test', () => { });
-  // Add more tests as needed
+import { repka } from '../../repka';
+import { watch } from '../index';
+
+describe('create watcher', () => {
+  const watcherSpy = jest.fn();
+  const state = repka<{foo: number}>({ foo: 1 });
+
+  const watchForFoo = async () => {
+    await watch(state, 'foo');
+    watcherSpy()
+  }
+
+  beforeEach(() => {
+    watcherSpy.mockClear();
+  });
+
+  test('spy called after foo updates', () => { 
+    watchForFoo();
+
+    state.foo = 2
+
+    setTimeout(() => {
+      expect(watcherSpy).toHaveBeenCalledTimes(1);
+    }, 0)
+  });
 });
