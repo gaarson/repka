2fa0888e1616ff24f7281e946e6668b966a306f9
Ruslan Idrisov <r.idrisov@alphaone.ru>
2025-10-06 06:33:52 +0300

    hotfixes

--- dist/react-provider/index.js
+++ dist/react-provider/index.js
@@ -1,1 +1,1 @@
-import{a}from"../chunks/3WQVPN7Q.chunk.js";import"../chunks/KFMMTQSC.chunk.js";export{a as simpleReactProvider};
+import{a}from"../chunks/427ZBHGA.chunk.js";import"../chunks/KFMMTQSC.chunk.js";export{a as simpleReactProvider};
