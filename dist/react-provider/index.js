20023af020e586ee3bf4b3ded6efb513650688be
Ruslan Idrisov <r.idrisov@alphaone.ru>
2025-10-23 23:53:35 +0300

    change README remove

--- dist/react-provider/index.js
+++ dist/react-provider/index.js
@@ -1,1 +1,1 @@
-import{a,b}from"../chunks/AUP5IPF4.chunk.js";import"../chunks/LGHY6XNV.chunk.js";import"../chunks/O3VCMV5J.chunk.js";export{a as simpleHook,b as simpleReactProvider};
+import{a,b}from"../chunks/C4CFSTAA.chunk.js";import"../chunks/LGHY6XNV.chunk.js";import"../chunks/O3VCMV5J.chunk.js";export{a as simpleHook,b as simpleReactProvider};
